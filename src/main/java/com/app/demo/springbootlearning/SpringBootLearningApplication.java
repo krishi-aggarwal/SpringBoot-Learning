993d76345145e3986d1a2e4c2adf80b7503a05c3
krishi-aggarwal <krishi.2004.aggarwal@gmail.com>
2026-08-05 21:43:54 +0530

    Springboot-Learning

---
+++ src/main/java/com/app/demo/springbootlearning/SpringBootLearningApplication.java
@@ -0,0 +1,21 @@
+package com.app.demo.springbootlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLearningApplication.class, args);
+    }
+
+    @GetMapping
+    public String hello(){
+        return "HelloWorld";
+    }
+
+}
